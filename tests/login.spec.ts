d3b67d0e0bf576830ad76ca229e03909cac45f3e
Rikata01 <wissarut.ba@gmail.com>
2026-05-05 19:26:06 +0700

    day2: add POM + login test cases

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,16 +1,29 @@
 import { test,expect } from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
 
 test('login with valid credentials', async ({ page }) => {
-    
-    await page.goto('https://www.saucedemo.com')
-    
-    await page.fill('#user-name', 'wrong_user')
-    
-    await page.fill('#password', 'wrong_sauce')
+    const loginPage = new LoginPage(page)
 
-    await page.click('#login-button')
+    await loginPage.goto()
+    await loginPage.login('standard_user', 'secret_sauce')
 
-    await expect(page.locator('[data-test="error"]')).toBeVisible()
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+})
 
-    // await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+test('login with invalid credentials', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+
+    await loginPage.goto()
+    await loginPage.login('wrong_user', 'wrong_pass')
+
+    await expect(await loginPage.getErrorMessage()).toBeVisible()
+})
+
+test('login with empty fields', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+
+    await loginPage.goto()
+    await loginPage.login('', '')
+
+    await expect(await loginPage.getErrorMessage()).toBeVisible()
 })
